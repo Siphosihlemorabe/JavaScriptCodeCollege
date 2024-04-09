60460e422cc25fcc82353ba78a9d99be28da9f2e
Siphosihlemorabe <152267979+Siphosihlemorabe@users.noreply.github.com>
2024-04-09 17:57:56 +0200

    Add files via upload

---
+++ chapter4/practise4,3.js
@@ -0,0 +1,6 @@
+
+let myId = true; 
+
+let message = isValidID ? "Welcome! You are allowed into the venue." : "Sorry, your ID is not valid. You are not allowed into the venue.";
+
+console.log(message);
